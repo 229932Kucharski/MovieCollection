1e98486a3ace3cd70f2863efdf8b08c989c8f3f3
229932 Grzegorz Kucharski <229932@p.lodz.pl>
2021-03-07 21:58:35 +0100

    Dodano zaleznosci javafx do pom.xml. Utworzono klase startowa App oraz kontroler do glownego okna

---
+++ src/main/java/app/App.java
@@ -0,0 +1,25 @@
+package app;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class App extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("/mainWindow.fxml"));
+        AnchorPane anchorPane = loader.load();
+        Scene scene = new Scene(anchorPane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
