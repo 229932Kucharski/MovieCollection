6dc27ef7d84350d3c8ede9a26afdb76b19b0b4af
229932 Grzegorz Kucharski <229932@p.lodz.pl>
2021-03-25 23:57:24 +0100

    Implementacja klasy abstrakcyjnej Account. Dodano JUnit do zaleznosci projektu

--- src/main/java/model/account/Account.java
+++ src/main/java/model/account/Account.java
@@ -1,4 +1,34 @@
 package model.account;
 
-public class Account {
+import java.util.Date;
+
+public abstract class Account {
+
+    private final String name;
+    private byte[] password;
+    private final Date registerDate;
+
+    public Account(String name, byte[] password) {
+        this.name = name;
+        this.password = password;
+        registerDate = new Date();
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public byte[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(byte[] password) {
+        this.password = password;
+    }
+
+    public Date getDate() {
+        return registerDate;
+    }
+
 }
