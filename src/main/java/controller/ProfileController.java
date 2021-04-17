97098020135ced3e6db3d44ce27862f82dfe9c1b
Gelo2424 <O@Oleg2424>
2021-04-18 00:08:38 +0200

    dodanie komunikatow do zmieny hasla

--- src/main/java/controller/ProfileController.java
+++ src/main/java/controller/ProfileController.java
@@ -2,12 +2,12 @@ package controller;
 
 import app.App;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonType;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.account.password.PasswordHashing;
 import model.account.user.Adult;
 import model.account.user.User;
 import model.dao.JdbcUserDao;
@@ -15,6 +15,8 @@ import model.dao.JdbcUserDao;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class ProfileController {
     public Text nameText;
@@ -23,10 +25,17 @@ public class ProfileController {
     public Text emailText;
     public Text phoneNumber;
     public AnchorPane profileAnchorPane;
+    public VBox passwordChangeVBox;
+    public PasswordField oldPassTexField;
+    public PasswordField newPassTextField;
+    public PasswordField newPassRepTextField;
+    public Button savePassword;
+    public Text passwordWarning;
     User user;
 
 
     public void initialize(){
+        passwordChangeVBox.setVisible(false);
         user = UserController.getUser();
         nameText.setText(user.getName());
         ageText.setText(String.valueOf(Period.between(user.getBirthDate(), LocalDate.now()).getYears()));
@@ -64,7 +73,37 @@ public class ProfileController {
         });
     }
 
+    private void setPasswordWarning(String mess) {
+        passwordWarning.setText(mess);
+        passwordWarning.setStyle("-fx-text-fill: red");
+    }
+
     public void back() {
         App.changeScene(profileAnchorPane, "mainWindow");
+    }
+
+    public void savePassword() {
+        if(oldPassTexField.getText().equals("") || newPassTextField.getText().equals("")
+                || newPassRepTextField.getText().equals("")) {
+            setPasswordWarning("The field cant be empty");
+            return;
+        }
+        if(!Arrays.equals(PasswordHashing.hashPassword(oldPassTexField.getText()), user.getPassword())) {
+            setPasswordWarning("Old password is incorrect");
+            return;
+        }
+        if(!newPassTextField.getText().equals(newPassRepTextField.getText())) {
+            setPasswordWarning("New password is not the same");
+            return;
+        }
+        if(newPassTextField.getText().length() < 6) {
+            setPasswordWarning("Password is too short");
+            return;
+        }
+        setPasswordWarning("Password has been changed");
+    }
+
+    public void changePassword() {
+        passwordChangeVBox.setVisible(true);
     }
 }
