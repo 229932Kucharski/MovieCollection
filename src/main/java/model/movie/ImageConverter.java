2c716bfcdcf712361095105217b175c210958240
229932 Grzegorz Kucharski <229932@p.lodz.pl>
2021-05-15 21:31:07 +0200

    Komentarze do ImageConverter, zmiana w filmWindow.fxml

--- src/main/java/model/movie/ImageConverter.java
+++ src/main/java/model/movie/ImageConverter.java
@@ -9,8 +9,12 @@ import java.io.IOException;
 
 public class ImageConverter {
 
+    /**
+     * Convert image to byte array
+     * @param movieId movie id that you want to get image
+     * @return image in byte array
+     */
     public static byte[] imageToByteArray(int movieId) throws IOException {
-
         File file = new File("src/main/assets/img/movieCover/" + movieId + ".jpg");
         BufferedImage bImage= ImageIO.read(file);
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -18,8 +22,12 @@ public class ImageConverter {
         return bos.toByteArray();
     }
 
+    /**
+     * Convert image to byte array
+     * @param path path to image
+     * @return image in byte array
+     */
     public static byte[] imageToByteArray(String path) throws IOException {
-
         File file = new File(path);
         BufferedImage bImage= ImageIO.read(file);
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -27,9 +35,18 @@ public class ImageConverter {
         return bos.toByteArray();
     }
 
+    /**
+     * Convert byte array to iamge
+     * @param movieId id of movie that images belong to
+     * @param data image in byte array
+     */
     public static void byteArrayToImage(int movieId, byte[] data) throws IOException {
         ByteArrayInputStream bis = new ByteArrayInputStream(data);
         BufferedImage bImage = ImageIO.read(bis);
+        File file = new File("src/main/assets/img/movieCover/" + movieId + ".jpg");
+        if(!file.exists()) {
+            file.mkdirs();
+        }
         ImageIO.write(bImage, "jpg", new File("src/main/assets/img/movieCover/" + movieId + ".jpg"));
     }
 
